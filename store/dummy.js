b583efc8085df089e1e63d16dd349f102915b6d0
Ruben Toro <rubensdt01@gmail.com>
2022-06-17 22:21:11 -0500

    the course resumes, modules users, network, first route

---
+++ store/dummy.js
@@ -0,0 +1,19 @@
+const db = {};
+
+function list(tabla){
+    return db[tabla];
+}
+function get(tabla, id){
+    let col = list(tabla)
+    return col.filter(item => item.id === id)[0] || null;
+}
+function upsert(tabla, data){
+    db[collection].push(data);
+}
+function remove (tabla, id){
+    return true;
+}
+
+module.exports = {
+    list, get, upsert, remove,
+}
